c73849fd6ed2c83c349c20b1dc8e757492071bbf
Shashi <LENOVO@DESKTOP-2MREFRI>
2023-07-24 09:55:27 +0530

    Refractor: Object created globally

--- src/test/java/com/ttt/tdd/TicTacToeTest.java
+++ src/test/java/com/ttt/tdd/TicTacToeTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 public class TicTacToeTest {
-	
+
+	TicTacToe ticTacToe = new TicTacToe();
+
 	@Test
 	public void initializeNewTicTacToeGame() {
-		TicTacToe ticTacToe = new TicTacToe();
 		assertNotNull(ticTacToe);
 	}
+
 }
