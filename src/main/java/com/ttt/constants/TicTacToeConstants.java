6862cbc8b1f47fa6aca51bb1aa7ace958f3bc8ab
Shashi <LENOVO@DESKTOP-2MREFRI>
2023-07-24 13:57:23 +0530

    Add: Created constants file

---
+++ src/main/java/com/ttt/constants/TicTacToeConstants.java
@@ -0,0 +1,13 @@
+package com.ttt.constants;
+
+public class TicTacToeConstants {
+
+	public static final Character FIRST_PLAYER = 'X';
+	public static final Character SECOND_PLAYER = 'O';
+	public static final String FIELD_IS_OCCUPIED = "Field is occupied!";
+	public static final String ERROR_MSG_OF_X_VALUE_IF_OUTSIDE_THE_BOARD = "X value is outside the board!";
+	public static final String ERROR_MSG_OF_Y_VALUE_IF_OUTSIDE_THE_BOARD = "Y value is outside the board!";
+	public static final String X_IS_THE_WINNER = "X is the Winner";
+	public static final String O_IS_THE_WINNER = "O is the Winner";
+	public static final String ALL_FIELDS_ARE_FILLED_SO_ITS_DRAW = "It is a draw";
+}
